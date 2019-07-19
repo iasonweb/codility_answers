f2a4fb7484d0f82ffd87bb7bb284723053f8159c
jeffbolton <i.otkhozoria@yahoo.com>
2019-07-19 17:36:02 +0400

    Added 'ChocolatesByNumbers.java'

--- java_solutions/ChocolatesByNumbers.java
+++ java_solutions/ChocolatesByNumbers.java
@@ -1,5 +1,19 @@
 public class ChocolatesByNumbers {
 
+    public int solution(int N, int M) {
+        int i = 0;
+        int counter = 0;
+        int[] arr = new int[N];
+        while (arr[i] == 0) {
+            arr[i] = 1;
+            counter += 1;
+            i = (i + M) % N;
+        }
+        return counter;
+    }
 
-    
+    public static void main(String[] args) {
+        ChocolatesByNumbers cbn = new ChocolatesByNumbers();
+        cbn.solution(10, 4);
+    }
 }
