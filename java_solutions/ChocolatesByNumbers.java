2ffe837f8df0cf46a6d15e28547e95ad8c230900
Iason Otk <i.otkhozoria@yahoo.com>
2019-09-09 19:00:23 +0400

    Add files via upload

--- java_solutions/ChocolatesByNumbers.java
+++ java_solutions/ChocolatesByNumbers.java
@@ -1,19 +1,30 @@
 public class ChocolatesByNumbers {
 
+class Solution {
     public int solution(int N, int M) {
-        int i = 0;
-        int counter = 0;
-        int[] arr = new int[N];
-        while (arr[i] == 0) {
-            arr[i] = 1;
-            counter += 1;
-            i = (i + M) % N;
+        if (M > N) {
+            M %= N;
         }
+        int counter = 0;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) a[i] = i;
+        int j = 0;
+        while (a[j] != -1) {
+            counter += 1;
+            a[j] = -1;
+            j += M;
+            if (j > (N - 1)) j %= N;
+        }
+        
         return counter;
     }
-
+}
+    
     public static void main(String[] args) {
-        ChocolatesByNumbers cbn = new ChocolatesByNumbers();
-        cbn.solution(10, 4);
+        System.out.println(solution(10, 4));
+        System.out.println();
+        System.out.println(solution(12, 23));
+        System.out.println(solution(12, 11));
     }
+
 }
